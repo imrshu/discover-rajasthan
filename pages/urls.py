68d1f54bbb231662c02f053ae5921d6504479e94
imrshu <rishabh.verma11998@gmail.com>
2020-08-23 22:33:22 +0530

    Fix: contact page ui

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'pages'
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('about_us', aboutUs, name="about"),
+    path('contact_us', contactUs, name="contact"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def home(request):
     if request.method == 'GET':
         return render(request, 'index.html')
+
+
+def aboutUs(request):
+    if request.method == 'GET':
+        return render(request, 'about.html')
+
+
+def contactUs(request):
+    if request.method == 'GET':
+        return render(request, 'contacts.html')
